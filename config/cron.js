c88d12586bf51db4d5a688d0aba8dbe39f10a246
lfelipeqn <luis.quinones@ingeniocontenido.co>
2020-07-17 11:08:10 -0500

    -Configuracion de Actualizacion de Pedidos Dafiti en cron

--- config/cron.js
+++ config/cron.js
@@ -8,13 +8,13 @@ module.exports.cron = {
       let integrations = await Integrations.find({channel:'dafiti'});
       integrations.forEach(async integration =>{
         try{
-          let statuses = [/*'pending'*/,'shipped','delivered','returned','canceled','failed'];
+          let statuses = ['pending','shipped','delivered','returned','canceled','failed'];
           let parameters=[];
           statuses.forEach(async state =>{
             if(state==='pending'){
               parameters= ['CreatedBefore='+moment().toISOString(true),'CreatedAfter='+moment().subtract(2,'hours').toISOString(true),'Status='+state,'SortDirection=ASC']
             }else{
-              parameters= ['UpdatedBefore='+moment('2020-07-14 23:59:59').toISOString(true),'UpdatedAfter='+moment('2020-07-14 00:00:00').subtract(2,'hours').toISOString(true),'Status='+state,'SortDirection=ASC']
+              parameters= ['UpdatedBefore='+moment(/*'2020-07-14 23:59:59'*/).toISOString(true),'UpdatedAfter='+moment(/*'2020-07-14 00:00:00'*/).subtract(2,'hours').toISOString(true),'Status='+state,'SortDirection=ASC']
             }
             await sails.helpers.channel.dafiti.orders(integration.seller,parameters);
           });
