acc8d1eb381ab21340329dfd9f406e7d611782bb
Fastcode Dev <support@fastcodelab.com>
2020-09-16 14:42:16 -0500

    restore custom

--- config/custom.js
+++ config/custom.js
@@ -10,7 +10,7 @@
 //IMPORT_MICROSERVICE  : 'https://import.1ecommerce.app:9000/graphql',
 
 module.exports.custom = {
-  IMPORT_MICROSERVICE  : 'http://localhost:9000/graphql',
+  IMPORT_MICROSERVICE  : 'https://import.1ecommerce.app:9000/graphql',
   PRODUCT_TYPE: 'Product',
   PRODUCT_VARIATION: 'ProductVariation',
   IMAGE_TYPE: 'ProductImage',
