19fa8a0d27f5be4ccb0e05ea71c78597f96e98d9
Pedro Luis Holguin <dropeholguin1994@gmail.com>
2020-09-22 07:29:54 -0500

    -Se valido existencia de categorio en el predictor de mercadolibre

--- api/helpers/channel/mercadolibre/find-category.js
+++ api/helpers/channel/mercadolibre/find-category.js
@@ -19,7 +19,11 @@ module.exports = {
   fn: async function (inputs,exits) {
     inputs.ml.get('sites/MCO/domain_discovery/search?limit=1&q='+encodeURIComponent(inputs.categories), (error,response) =>{
       if(error){return exits.error(error);}
-      return exits.success(response[0].category_id);
+      if (response.length > 0) {
+        return exits.success(response[0].category_id);
+      } else {
+        return exits.error('Categoria no Localizada');
+      }
     });
   }
 };
