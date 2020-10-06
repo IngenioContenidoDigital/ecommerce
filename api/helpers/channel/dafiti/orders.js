a66c2772a435181af18895b253a64d3e8b15122d
lfelipeqn <luis.quinones@ingeniocontenido.co>
2020-10-06 06:57:58 -0500

    -Eliminar . en DNI de usuario

--- api/helpers/channel/dafiti/orders.js
+++ api/helpers/channel/dafiti/orders.js
@@ -43,7 +43,7 @@ module.exports = {
                 password:await sails.helpers.passwords.hashPassword(order.NationalRegistrationNumber),
                 fullName:order.CustomerFirstName+' '+order.CustomerLastName,
                 dniType:'CC',
-                dni:order.NationalRegistrationNumber,
+                dni:order.NationalRegistrationNumber.replace(/\./gi,''),
                 mobilecountry:city[0].region.country,
                 mobile:order.AddressShipping.Phone2 ? parseInt(order.AddressShipping.Phone2) : 0,
                 mobileStatus:'unconfirmed',
