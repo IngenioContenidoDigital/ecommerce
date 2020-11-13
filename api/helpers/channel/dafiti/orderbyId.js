d95ff020fa5ddcf2f524362dd61d258ee17e3062
Ingenio Contenido Digital <luis.quinones@ingeniocontenido.co>
2020-11-12 20:57:03 -0500

    -Actualizar Condicion Where ProductVariation

--- api/helpers/channel/dafiti/orderbyId.js
+++ api/helpers/channel/dafiti/orderbyId.js
@@ -80,7 +80,7 @@ module.exports = {
                   items['OrderItem'].push(rs.SuccessResponse.Body.OrderItems.OrderItem);
                   
                   for(let item of items.OrderItem){
-                    let productvariation = await ProductVariation.findOne({or : [
+                    let productvariation = await ProductVariation.find({or : [
                       { id:item.Sku },
                       { reference: item.Sku }
                     ]})
@@ -91,8 +91,8 @@ module.exports = {
                     if(productvariation){
                       await CartProduct.create({
                         cart:cart.id,
-                        product:productvariation.product,
-                        productvariation:productvariation.id,
+                        product:productvariation[0].product,
+                        productvariation:productvariation[0].id,
                         totalDiscount:parseFloat(item.VoucherAmount),
                         totalPrice:parseFloat(item.ItemPrice),
                         externalReference:item.OrderItemId
--- api/helpers/channel/linio/orders.js
+++ api/helpers/channel/linio/orders.js
@@ -83,15 +83,15 @@ module.exports = {
                   items['OrderItem'].push(rs.SuccessResponse.Body.OrderItems.OrderItem);
                 }
                 for(let item of items.OrderItem){
-                  let productvariation = await ProductVariation.findOne({or : [
+                  let productvariation = await ProductVariation.find({or : [
                     { id:item.Sku },
                     { reference: item.Sku }
                   ]});
                   if(productvariation){
                     await CartProduct.create({
                       cart: cart.id,
-                      product: productvariation.product,
-                      productvariation: productvariation.id,
+                      product: productvariation[0].product,
+                      productvariation: productvariation[0].id,
                       totalDiscount: parseFloat(item.VoucherAmount),
                       totalPrice: parseFloat(item.ItemPrice),
                       externalReference: item.OrderItemId
