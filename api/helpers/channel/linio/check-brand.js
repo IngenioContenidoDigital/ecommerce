d5f0d3d2a381634da29023c338c2aaae13f46675
Ingenio Contenido Digital <luis.quinones@ingeniocontenido.co>
2020-12-12 08:36:57 -0500

    -Actualizar Nombre de Marca en Linio

--- api/helpers/channel/linio/check-brand.js
+++ api/helpers/channel/linio/check-brand.js
@@ -6,10 +6,6 @@ module.exports = {
       type:'string',
       required:true,
     },
-    seller:{
-      type:'string',
-      required:true,
-    }
   },
   exits: {
     success: {
@@ -18,8 +14,22 @@ module.exports = {
   },
   fn: async (inputs,exits) => {
     let brand = 'Generico';
-    let sign = await sails.helpers.channel.linio.sign('GetBrands', inputs.seller);
+    let integrations = await Integrations.find({channel:'linio'});
+    let sign = await sails.helpers.channel.linio.sign('GetBrands', integrations[0].seller);
 
+    inputs.brand=inputs.brand.replace(/[^a-zA-Z0-9_-\s]/g,'');
+
+    switch(inputs.brand.toLowerCase()){
+      case 'rosé pistol':
+        inputs.brand = 'rose pistol';
+        break;
+      case '7 de color siete':
+        inputs.brand = 'color siete';
+        break;
+      case 'color siete care':
+        inputs.brand = 'color siete';
+        break;
+    }
     await sails.helpers.request('https://sellercenter-api.linio.com.co','/?'+sign,'GET')
     .then(async (resData)=>{
       let result = JSON.parse(resData);
