b6188f61338f128300e85c896574f62f59ec7cb2
lfelipeqn <luis.quinones@ingeniocontenido.co>
2021-02-03 15:34:11 -0500

    -Ajuste Horario Cron SIESA

--- config/cron.js
+++ config/cron.js
@@ -50,7 +50,7 @@ module.exports.cron = {
     timezone: 'America/Bogota'
   },
   SiesaOrdersStatusChanged: {
-    schedule: '50 20 */3 * * *',
+    schedule: '50 */20 * * * *',
     onTick: async () => {
       console.log('Iniciando Rastreo de Pedidos Siesa');
       let moment = require('moment');
