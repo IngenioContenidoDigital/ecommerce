3a7d1646f3bf483c0eca425b2004be16784bd26d
David <felipe0160@gmail.com>
2021-02-06 10:31:34 -0500

    channel sync

--- api/helpers/channel/channel-sync.js
+++ api/helpers/channel/channel-sync.js
@@ -18,7 +18,6 @@ module.exports = {
     if (productChannels.length > 0) {
       for (const item of productChannels) {
         let integration = await Integrations.findOne({id: item.integration}).populate('channel');
-
         if (integration.channel.name === 'dafiti' && item.status) {
           let result = await sails.helpers.channel.dafiti.product([inputs.product], item.price);
           const xml = jsonxml(result,true);
@@ -40,6 +39,57 @@ module.exports = {
           const xml = jsonxml(result,true);
           let sign = await sails.helpers.channel.linio.sign(integration.id,'ProductUpdate',inputs.product.seller);
           await sails.helpers.request(integration.channel.endpoint,'/?'+sign,'POST',xml);
+        }
+        if (integration.channel.name === 'muybacano' && item.status) {
+          let axios = require('axios');
+          try { 
+            let productvariations = await ProductVariation.find({product:inputs.product.id}).populate('variation');
+            for (let index = 0; index < productvariations.length; index++) {
+              const pv = productvariations[index];
+              options = {
+                method: 'post',
+                url: `${integration.channel.endpoint}/api/catalog_system/pvt/skuSeller/changenotification/${integration.seller}/${pv.id}`,
+                headers: {
+                    'X-VTEX-API-AppToken':integration.user,
+                    'X-VTEX-API-AppKey':integration.key,
+                    'content-type': 'application/json',
+                    accept: 'application/json'
+                }
+              };
+              await axios(options).catch((e) => {console.log(e.response.status);});
+              let body = await sails.helpers.channel.muybacano.product(product.id, pv.id, 'Post', item.price, {}, item.status);
+              options = {
+                method: 'put',
+                url: `https://api.vtex.com/muybacano/suggestions/${integration.seller}/${pv.id}`,
+                headers: {
+                    'X-VTEX-API-AppToken':integration.user,
+                    'X-VTEX-API-AppKey':integration.key,
+                    'content-type': 'application/json',
+                    accept: 'application/json'
+                },
+                data:body
+             }
+             await axios(options).catch((e) => {console.log(e.response.status);});
+            }
+          } catch (err) {
+            console.log(err);
+          }
+        }
+        if (integration.channel.name === 'coppel' && item.status) {
+          let axios = require('axios');
+          let body = await sails.helpers.channel.coppel.product(inputs.product.id, 'Update', parseFloat(item.price), item.status)
+          .intercept((err) => {return new Error(err.message);});
+          options = {
+            method: 'post',
+            url: `${integration.channel.endpoint}api/offers`,
+            headers: {
+                'Authorization':`${integration.key}`,
+                'content-type': `application/json`,
+                accept: 'application/json'
+            },
+            data:body
+          }
+          let response_offer = await axios(options).catch((e) => {result=e.response, console.log(result);});
         }
       }
     }
