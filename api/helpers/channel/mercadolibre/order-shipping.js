a8e6be2903113dffdf41f71a1960c0842eb1ed13
Pedro Luis Holguin <dropeholguin1994@gmail.com>
2021-02-10 14:44:41 -0500

    Se corrigen bugs en webhook de crear ordenes en meli.

---
+++ api/helpers/channel/mercadolibre/order-shipping.js
@@ -0,0 +1,80 @@
+module.exports = {
+  friendlyName: 'Order Shipping',
+  description: 'Helper usado para realizar la creación de orden item cuando hay mas de una orden con el mismo shipping',
+  inputs: {
+    data:{
+      type:'ref',
+      required:true,
+    }
+  },
+  exits: {
+    success: {
+      description: 'All done.',
+    },
+  },
+  fn: async function (inputs, exits) {
+    let moment = require('moment');
+    let order = inputs.data.order;
+    let cartproducts = inputs.data.cartProducts;
+    let sellers = [];
+    cartproducts.forEach(cp=>{
+      if(!sellers.includes(cp.product.seller)){
+        sellers.push(cp.product.seller);
+      }
+    });
+
+    for(let seller of sellers){
+      let sellerproducts = cartproducts.filter(cp => cp.product.seller === seller);
+      if(sellerproducts.length>0){
+        let carttotal = 0;
+        let productsdiscount = 0;
+        sellerproducts.forEach(cp=>{
+          carttotal+= cp.totalPrice;
+          productsdiscount+=cp.totalDiscount;
+        });
+
+        try{
+          await Order.updateOne({id:order.id}).set({
+            totalOrder: order.totalOrder + carttotal,
+            totalProducts: order.totalProducts + carttotal,
+            productsDiscount: order.productsDiscount + productsdiscount
+          });
+
+          const resultseller = await Seller.findOne({id:order.seller});
+          const commissionDiscount = await CommissionDiscount.find({
+            where:{
+              to:{'>=':moment().valueOf()},
+              from:{'<=':moment().valueOf()},
+              seller: seller
+            },
+            sort: 'createdAt DESC',
+            limit: 1
+          });
+
+          for(let cp of sellerproducts){
+            await OrderItem.create({
+              order:order.id,
+              product:cp.product.id,
+              productvariation:cp.productvariation.id,
+              price:cp.totalPrice,
+              discount:cp.totalDiscount,
+              originalPrice:cp.productvariation.price,
+              externalReference:cp.externalReference,
+              commission: commissionDiscount.length > 0 ? commissionDiscount[0].value : resultseller.salesCommission
+            });
+            let pv = await ProductVariation.findOne({id:cp.productvariation.id});
+            if(pv){
+              let quantity = pv.quantity-=1;
+              let updated = await ProductVariation.updateOne({id:pv.id}).set({quantity: quantity < 0 ? 0 : quantity});
+              let uproduct = await Product.findOne({id:updated.product});
+              await sails.helpers.channel.channelSync(uproduct);
+            }
+          }
+        }catch(err){
+          return exits.error(err);
+        }
+      }
+    }
+    return exits.success();
+  }
+};
--- api/helpers/channel/mercadolibre/orders.js
+++ api/helpers/channel/mercadolibre/orders.js
@@ -36,46 +36,83 @@ module.exports = {
               return exits.error(err.message);
             });
             if(shipping){
-              let cityname = shipping['receiver_address'].city.name.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-              let state = shipping['receiver_address'].state.name.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-              if(cityname==='Bogota D.C.' ){cityname='Bogota';}
-              if(state==='Bogota D.C.'){state='Bogota';}
-              let city = await City.find({name:[cityname.toLowerCase().trim(), state.toLowerCase().trim()]}).populate('region');
-              if(city && oexists.length === 0){
-                let user = await User.findOrCreate({emailAddress:order.buyer.email},{
-                  emailAddress:order.buyer.email,
-                  emailStatus:'confirmed',
-                  password:await sails.helpers.passwords.hashPassword(order.buyer['billing_info']['doc_number']),
-                  fullName:order.buyer['first_name']+' '+order.buyer['last_name'],
-                  dniType:'CC',
-                  dni:order.buyer['billing_info']['doc_number'],
-                  mobilecountry:city[0].region.country,
-                  mobile:0,
-                  mobileStatus:'unconfirmed',
-                  profile:profile.id
-                });
-                let address = await Address.findOrCreate({addressline1:shipping['receiver_address']['address_line'].toLowerCase().trim()},{
-                  name:shipping['receiver_address']['address_line'].trim().toLowerCase(),
-                  addressline1:shipping['receiver_address']['address_line'].trim().toLowerCase(),
-                  addressline2:shipping['receiver_address']['comment'] ? shipping['receiver_address']['comment'].trim().toLowerCase() : '',
-                  country:city[0].region.country,
-                  region:city[0].region.id,
-                  city:city[0].id,
-                  notes:shipping['receiver_address']['comment'] ? shipping['receiver_address']['comment'].trim().toLowerCase() : '',
-                  zipcode:shipping['receiver_address']['zip_code'] ? shipping['receiver_address']['zip_code'] : '',
-                  user:user.id,
-                });
-                let payment = {
-                  data:{
-                    estado:'Aceptado',
-                    channel:'mercadolibre',
-                    channelref:order.id,
-                    integration:integration.id
-                  }
-                };
+              let existShipping = await Order.find({channel:'mercadolibre', tracking: order.shipping.id, integration: integration.id});
+              if (existShipping.length === 0) {
+                let cityname = shipping['receiver_address'].city.name.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+                let state = shipping['receiver_address'].state.name.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+                if(cityname==='Bogota D.C.' ){cityname='Bogota';}
+                if(state==='Bogota D.C.'){state='Bogota';}
+                let city = await City.find({name:[cityname.toLowerCase().trim(), state.toLowerCase().trim()]}).populate('region');
+                if(city && oexists.length === 0){
+                  let user = await User.findOrCreate({emailAddress:order.buyer.email},{
+                    emailAddress:order.buyer.email,
+                    emailStatus:'confirmed',
+                    password:await sails.helpers.passwords.hashPassword(order.buyer['billing_info']['doc_number']),
+                    fullName:order.buyer['first_name']+' '+order.buyer['last_name'],
+                    dniType:'CC',
+                    dni:order.buyer['billing_info']['doc_number'],
+                    mobilecountry:city[0].region.country,
+                    mobile:0,
+                    mobileStatus:'unconfirmed',
+                    profile:profile.id
+                  });
+                  let address = await Address.findOrCreate({addressline1:shipping['receiver_address']['address_line'].toLowerCase().trim()},{
+                    name:shipping['receiver_address']['address_line'].trim().toLowerCase(),
+                    addressline1:shipping['receiver_address']['address_line'].trim().toLowerCase(),
+                    addressline2:shipping['receiver_address']['comment'] ? shipping['receiver_address']['comment'].trim().toLowerCase() : '',
+                    country:city[0].region.country,
+                    region:city[0].region.id,
+                    city:city[0].id,
+                    notes:shipping['receiver_address']['comment'] ? shipping['receiver_address']['comment'].trim().toLowerCase() : '',
+                    zipcode:shipping['receiver_address']['zip_code'] ? shipping['receiver_address']['zip_code'] : '',
+                    user:user.id,
+                  });
+                  let payment = {
+                    data:{
+                      estado:'Aceptado',
+                      channel:'mercadolibre',
+                      channelref:order.id,
+                      integration:integration.id
+                    }
+                  };
 
-                payment.data['ref_payco'] = order.id;
-                let cart = await Cart.create().fetch();
+                  payment.data['ref_payco'] = order.id;
+                  let cart = await Cart.create().fetch();
+                  for(let item of order['order_items']){
+                    try{
+                      let productvariation;
+                      if(item.item['seller_sku']){
+                        productvariation = await ProductVariation.findOne({id:item.item['seller_sku']});
+                      }else{
+                        let pr = await Product.findOne({mlid:item.item.id}).populate('variations');
+                        productvariation = pr.variations[0];
+                      }
+                      if(productvariation){
+                        for (let i = 1; i <= item.quantity; i++) {
+                          await CartProduct.create({
+                            cart:cart.id,
+                            product:productvariation.product,
+                            productvariation:productvariation.id,
+                            totalDiscount:parseFloat(0),
+                            totalPrice:parseFloat(item['full_unit_price']),
+                            externalReference:item.item['variation_id'] ? item.item['variation_id'] : ''
+                          });
+                        }
+                      }
+                    }catch(err){
+                      return exits.error(err.message);
+                    }
+                  }
+                  if((await CartProduct.count({cart:cart.id}))>0){
+                    let carrier = shipping['tracking_method'].split(' ');
+                    let corders = await sails.helpers.order({address:address,user:user,cart:cart,method:order.payments[0].payment_method_id,payment:payment,carrier:carrier[0]});
+                    await Order.updateOne({id:corders[0].id}).set({createdAt:parseInt(moment(order['date_created']).valueOf()),tracking:shipping.id});
+                  }
+                }else{
+                  return exits.error('Ciudad No Localizada');
+                }
+              } else if(existShipping.length > 0){
+                let cartProducts = [];
                 for(let item of order['order_items']){
                   try{
                     let productvariation;
@@ -86,26 +123,24 @@ module.exports = {
                       productvariation = pr.variations[0];
                     }
                     if(productvariation){
-                      await CartProduct.create({
-                        cart:cart.id,
-                        product:productvariation.product,
-                        productvariation:productvariation.id,
-                        totalDiscount:parseFloat(0),
-                        totalPrice:parseFloat(item['full_unit_price']),
-                        externalReference:item.item['variation_id'] ? item.item['variation_id'] : ''
-                      });
+                      for (let i = 1; i <= item.quantity; i++) {
+                        let cartproduct = await CartProduct.create({
+                          cart: existShipping[0].cart,
+                          product:productvariation.product,
+                          productvariation:productvariation.id,
+                          totalDiscount:parseFloat(0),
+                          totalPrice:parseFloat(item['full_unit_price']),
+                          externalReference:item.item['variation_id'] ? item.item['variation_id'] : ''
+                        }).fetch();
+                        cartproduct = await CartProduct.findOne({id: cartproduct.id}).populate('product').populate('productvariation');
+                        cartProducts.push(cartproduct);
+                      }
                     }
                   }catch(err){
                     return exits.error(err.message);
                   }
                 }
-                if((await CartProduct.count({cart:cart.id}))>0){
-                  let carrier = shipping['tracking_method'].split(' ');
-                  let corders = await sails.helpers.order({address:address,user:user,cart:cart,method:order.payments[0].payment_method_id,payment:payment,carrier:carrier[0]});
-                  await Order.updateOne({id:corders[0].id}).set({createdAt:parseInt(moment(order['date_created']).valueOf()),tracking:shipping.id});
-                }
-              }else{
-                return exits.error('Ciudad No Localizada');
+                await sails.helpers.channel.mercadolibre.orderShipping({cartProducts, order: existShipping[0]});
               }
             }
           }else{
