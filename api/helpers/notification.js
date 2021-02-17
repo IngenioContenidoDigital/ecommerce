ca61be328d7977b35d40bb116c0db1dc4a41cfdc
Ingenio Contenido Digital <luis.quinones@ingeniocontenido.co>
2021-02-16 19:40:14 -0500

    -BUG: Validacion de Estado de Orden de Acuerdo a parametro recibido

--- api/helpers/notification.js
+++ api/helpers/notification.js
@@ -19,16 +19,21 @@ module.exports = {
   },
   fn: async function (inputs, exits) {
     let users = await User.find({seller: inputs.seller});
-    let state = inputs.order.currentstatus;
-    let newstate = await OrderState.findOne({id:state.id});
+    let state = null;
+    if(inputs.order.currentstatus.id){
+      state = inputs.order.currentstatus;
+    }else{
+      state = await OrderState.findOne({id:inputs.order.currentstatus});
+    }
+    
     for (const user of users) {
       const country = await Country.findOne({id:user.mobilecountry});
       const userNotification = await UserNotification.findOne({user: user.id, state: state.id});
       if (userNotification && userNotification.sms) {
-        await sails.helpers.sendSms('Te informamos que se ha cambio de estado (' + newstate.name.toUpperCase() + ') la orden #'+ inputs.order.reference +'. Por favor verificar la orden.',country.prefix+user.mobile);
+        await sails.helpers.sendSms('Te informamos que se ha cambio de estado (' + state.name.toUpperCase() + ') la orden #'+ inputs.order.reference +'. Por favor verificar la orden.',country.prefix+user.mobile);
       }
       if(userNotification && userNotification.email){
-        await sails.helpers.sendEmail('email-notification',{state: newstate.name.toUpperCase(), order: inputs.order},user.emailAddress,'Cambio de Estado de un Pedido');
+        await sails.helpers.sendEmail('email-notification',{state: state.name.toUpperCase(), order: inputs.order},user.emailAddress,'Cambio de Estado de un Pedido');
       }
     }
     return exits.success();
